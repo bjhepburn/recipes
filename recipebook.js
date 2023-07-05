17c12c20c4964509fbfd8ee4cb93eb14e7559d03
Bradley Hepburn <119343728+bjhepburn@users.noreply.github.com>
2023-07-05 18:14:30 -0500

    initial commit

---
+++ recipebook.js
@@ -0,0 +1,7 @@
+const url = ""
+
+d3.json(url).then(response => {
+    let recipes = response;
+
+    let dish_type = docuemnt.getElementyId("selType")
+})
---
+++ recipes.js
@@ -0,0 +1,42 @@
+[
+        {
+            "name": "snick",
+            "type": "cookie",
+            "ingredients": [
+                "cup sugar",
+                " cup flour"
+            ],
+            "directions": [
+                "preheat",
+                " bake"
+            ]
+        },
+        {
+            "name": "brownies",
+            "type": "brownie",
+            "ingredients": [
+                "cocoa",
+                " flour",
+                " butter"
+            ],
+            "directions": [
+                "mix",
+                " bake",
+                " eat"
+            ]
+        },
+        {
+            "name": "test",
+            "type": "cookie",
+            "ingredients": [
+                "i1",
+                "i2",
+                "i3"
+            ],
+            "directions": [
+                "d1",
+                "d2",
+                "d3"
+            ]
+        }
+    ]
