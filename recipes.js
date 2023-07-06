c6beef6b6c1dc89f644592510dced6b47acf9d96
Bradley Hepburn <119343728+bjhepburn@users.noreply.github.com>
2023-07-06 17:17:36 -0500

    test run

--- recipes.js
+++ recipes.js
@@ -1,42 +1,62 @@
 [
-        {
-            "name": "snick",
-            "type": "cookie",
-            "ingredients": [
-                "cup sugar",
-                " cup flour"
-            ],
-            "directions": [
-                "preheat",
-                " bake"
-            ]
-        },
-        {
-            "name": "brownies",
-            "type": "brownie",
-            "ingredients": [
-                "cocoa",
-                " flour",
-                " butter"
-            ],
-            "directions": [
-                "mix",
-                " bake",
-                " eat"
-            ]
-        },
-        {
-            "name": "test",
-            "type": "cookie",
-            "ingredients": [
-                "i1",
-                "i2",
-                "i3"
-            ],
-            "directions": [
-                "d1",
-                "d2",
-                "d3"
-            ]
-        }
-    ]
+    {
+        "name": "snick",
+        "type": "cookie",
+        "ingredients": [
+            "cup sugar",
+            " cup flour"
+        ],
+        "directions": [
+            "preheat",
+            " bake"
+        ]
+    },
+    {
+        "name": "brownies",
+        "type": "brownie",
+        "ingredients": [
+            "cocoa",
+            " flour",
+            " butter"
+        ],
+        "directions": [
+            "mix",
+            " bake",
+            " eat"
+        ]
+    },
+    {
+        "name": "test",
+        "type": "cookie",
+        "ingredients": [
+            "i1",
+            "i2",
+            "i3"
+        ],
+        "directions": [
+            "d1",
+            "d2",
+            "d3"
+        ]
+    },
+    {
+        "name": "Cinnamon Rolls",
+        "type": "yeasted",
+        "ingredients": [
+            "1 cup warm milk",
+            " 2 1/2 tsp instant dry yeast",
+            " 2 large eggs(room temp)",
+            " 1/3 cup melted butter",
+            " 1/2 cup granulated sugar",
+            " 4 cups all-purpose flour"
+        ],
+        "directions": [
+            "Pour warm milk in mixing bowl and sprinkle with yeast. Wait 10 minutes.",
+            "Add eggs'",
+            "' butter and sugar. Mix until combined.",
+            "Add flour and mix until combined- let stand 5 minutes.",
+            "Knead with dough hook until sticky dough forms.",
+            "Put into greased bowl; cover in plastic wrap; let rise 1 hour in oven."
+        ]
+    }
+]
