fd13e611429c47f346abcfc4b094444e4a7f4a19
Bradley Hepburn <119343728+bjhepburn@users.noreply.github.com>
2023-07-07 12:55:05 -0500

    hope

--- recipes.js
+++ recipes.js
@@ -49,14 +49,31 @@
             "2 large eggs(room temp)",
             "1/3 cup melted butter",
             "1/2 cup granulated sugar",
-            "4 cups all-purpose flour"
+            "4 to 4 1/2 cups all-purpose flour",
+            "Filling",
+            "1/2 cup butter",
+            "1 cup brown sugar",
+            "2 Tbsp cinnamon",
+            "1/2 cup heavy cream(for pouring over risen rolls)",
+            "Frosting",
+            "6 ounces cream cheese",
+            "1/3 cup butter",
+            "2 cups powdered sugar",
+            "1/2 Tbsp Vanilla"
         ],
         "directions": [
             "Pour warm milk in mixing bowl and sprinkle with yeast. Wait 10 minutes.",
             "Add eggs butter and sugar. Mix until combined.",
             "Add flour and mix until combined- let stand 5 minutes.",
             "Knead with dough hook until sticky dough forms.",
-            "Put into greased bowl; cover in plastic wrap; let rise 1 hour in oven."
+            "Put into greased bowl; cover in plastic wrap; let rise 1 hour in oven.",
+            "Punch down and roll out on floured service (approximately 16\"x12\")",
+            "Spread butter over rolled out dough and cover with cinnamon-sugar mix",
+            "Roll then cut into 2 inch sections and place in buttered pan",
+            "Let rise another hour",
+            "Preheat oven to 350 F",
+            "Pour heavy cream over rolls and bake for 22 minutes.",
+            "Mix frosting in stand mixer and spread over warm rolls."
         ]
     }
 ]
