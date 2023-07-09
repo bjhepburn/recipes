5ec0391b014c78c68d36e5ff4d33200d73adb7f0
Bradley Hepburn <119343728+bjhepburn@users.noreply.github.com>
2023-07-09 15:39:36 -0500

    update

--- recipes.js
+++ recipes.js
@@ -1,45 +1,5 @@
 [
     {
-        "name": "snick",
-        "type": "cookie",
-        "ingredients": [
-            "cup sugar",
-            "cup flour"
-        ],
-        "directions": [
-            "preheat",
-            "bake"
-        ]
-    },
-    {
-        "name": "brownies",
-        "type": "brownie",
-        "ingredients": [
-            "cocoa",
-            "flour",
-            "butter"
-        ],
-        "directions": [
-            "mix",
-            "bake",
-            "eat"
-        ]
-    },
-    {
-        "name": "test",
-        "type": "cookie",
-        "ingredients": [
-            "i1",
-            "i2",
-            "i3"
-        ],
-        "directions": [
-            "d1",
-            "d2",
-            "d3"
-        ]
-    },
-    {
         "name": "Cinnamon Rolls",
         "type": "yeasted",
         "ingredients": [
@@ -74,6 +34,110 @@
             "Preheat oven to 350 F",
             "Pour heavy cream over rolls and bake for 22 minutes.",
             "Mix frosting in stand mixer and spread over warm rolls."
+        ]
+    },
+    {
+        "name": "Snicker Doodle",
+        "type": "cookie",
+        "ingredients": [
+            "Cookies",
+            "1 \ufffd cups white sugar",
+            "\ufffd cup butter- softened",
+            "\ufffd cup shortening",
+            "2 large eggs",
+            "2 tsp vanilla extract",
+            "2 \ufffd cups all-purpose flour",
+            "2 tsp cream of tartar",
+            "1 tsp baking soda",
+            "Cinnamon-Sugar Coating",
+            "2 Tbsp white sugar",
+            "2 tsp cinnamon"
+        ],
+        "directions": [
+            "Preheat the oven to 400 degrees F (200 degrees C).",
+            "Make cookies: Beat sugar - butter - shortening - eggs and vanilla in a large bowl until smooth and creamy.",
+            "Whisk flour - cream of tartar - baking soda and salt together in a separate bowl. Gradually mix dry ingredients mixture into the wet ingredients just until combined. Shape dough into walnut-sized balls.",
+            "Make cinnamon-sugar: Combine sugar and cinnamon in a small bowl or zip-top plastic bag.",
+            "Place dough balls in cinnamon-sugar and roll or shake until coated. Place 2 inches apart on ungreased baking sheets.",
+            "Bake in the preheated oven until set but not too hard- 8 to 10 minutes switching racks halfway through.",
+            "Remove from the oven and immediately transfer to wire racks to cool."
+        ]
+    },
+    {
+        "name": "Classic Texas Sheet Cake",
+        "type": "Sheet Cake",
+        "ingredients": [
+            "Cake",
+            "3 sticks butter",
+            "3 Tbsp cocoa",
+            "1 cup water",
+            "1/2 tsp salt",
+            "2 cup sugar",
+            "1 tsp baking soda",
+            "2 cup flour",
+            "2 eggs",
+            "1/2 cup buttermilk",
+            "1 tsp vanilla",
+            "Icing",
+            "1 stick butter",
+            "3 Tbsp cocoa",
+            "5 Tbsp milk",
+            "1 tsp vanilla",
+            "1/2 cup chopped pecans",
+            "1 lb of 10x sugar"
+        ],
+        "directions": [
+            "Boil for 3 minutes: butter- cocoa- water- salt",
+            "whisk in sugar - flour and baking soda",
+            "When smooth whisk in eggs - vanilla and buttermilk.",
+            "Pour into sheet pan and bake at 350 for 30 minutes.",
+            "For the icing boil butter cocoa and milk for 3 minutes then add vanilla.",
+            "Whisking vigorously add 1 loub of 10x sugar.",
+            "Stir in chopped pecans.",
+            "Pour over cake and spread evenly to edges quickly."
+        ]
+    },
+    {
+        "name": "Italian Bread",
+        "type": "yeasted",
+        "ingredients": [
+            "2 1/4 tsp active dry yeast",
+            "1 tsp sugar",
+            "1 cup water at 110F",
+            "2 1/2 cup flour",
+            "2 tsp olive oil",
+            "1 tsp salt"
+        ],
+        "directions": [
+            "In boal of stand mixer combine yeast sugar and water. Let stand until foamy (10 minutes).",
+            "Add 2 cups of flour and mix until shaggy dough.",
+            "Add 2 - 2 1/2 cups flour and knead on medium until elastic dough forms - minutes.",
+            "Shape dough into ball and put in oiled bowl and turn to coat. Cover in plastic wrap and let rise approximately 1 hour.",
+            "Shape into batard and place on lined baking sheet. Preheat oven to 400F.",
+            "Let rise 20 - 30 minutes- covered.Bake 22-23 minutes and immediatelly remove from tray."
+        ]
+    },
+    {
+        "name": "Tea Cakes",
+        "type": "cookie",
+        "ingredients": [
+            "Cookies",
+            "1 cup butter",
+            "1 tsp vanilla",
+            "6 Tbsp confectioners' sugar",
+            "2 cups flour",
+            "1 cup chopped walnuts",
+            "Coating",
+            "1/3 cup confectioners' sugar"
+        ],
+        "directions": [
+            "Preheat oven to 350.",
+            "In bowl cream butter and vanilla until smooth.",
+            "Combine confectioners' sugar and flour then stir into butter mixture until blended.",
+            "Mix in chopped nuts.",
+            "Roll into 1 inch balls.",
+            "Bake 2 inchaes apart on ungreased cookie sheet for 12 minutes.",
+            "When cool roll in confectioners' sugar."
         ]
     }
 ]
